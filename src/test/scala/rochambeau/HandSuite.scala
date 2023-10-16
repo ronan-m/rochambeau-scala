67d7ee8ee723cef85b8946db90566195dac93343
ronan-m <ronanm2@gmail.com>
2023-10-16 17:05:33 +0200

    Add tests

---
+++ src/test/scala/rochambeau/HandSuite.scala
@@ -0,0 +1,25 @@
+package rochambeau
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class HandSuite extends AnyFunSuite {
+
+  test("First hand is beaten") {
+    assertResult(true)(Rock.isBeatenBy(Paper))
+    assertResult(true)(Paper.isBeatenBy(Scissors))
+    assertResult(true)(Scissors.isBeatenBy(Rock))
+  }
+
+  test("First hand is not better") {
+    assertResult(false)(Rock.isBeatenBy(Rock))
+    assertResult(false)(Paper.isBeatenBy(Paper))
+    assertResult(false)(Scissors.isBeatenBy(Scissors))
+  }
+
+  test("First hand is better") {
+    assertResult(false)(Rock.isBeatenBy(Scissors))
+    assertResult(false)(Paper.isBeatenBy(Rock))
+    assertResult(false)(Scissors.isBeatenBy(Paper))
+  }
+
+}
